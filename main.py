778cd50ff0f1438bad8e64b2389c4e06a853db29
Username-08 <yashranjan08@gmail.com>
2021-07-29 08:06:30 +0530

    commit

--- main.py
+++ main.py
@@ -7,44 +7,54 @@ from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton
 
 
 class SchoolProjApp(MDApp):
-	dialog = None
-	def build(self):
-		self.theme_cls.theme_style = "Dark"
-		self.theme_cls.primary_palette = "Teal"
-		return Builder.load_file('login.kv')
-	def loggerchecker(self):
-		self.root.ids.welcome_label.text = f'Sup {self.root.ids.user.text}!'
+    dialog = None
 
-	def clearchecker(self):
-		self.root.ids.welcome_label.text = "WELCOME"		
-		self.root.ids.user.text = ""		
-		self.root.ids.password.text = ""	
-	
-	def show_alert_dialog(self):
-		if not self.dialog:
-			self.dialog = MDDialog(
-				title = "LOGGED IN",
-				text = f"you have logged in {self.root.ids.user.text} ",
-				buttons =[
-					MDFlatButton(
-						text="Cancel", on_release = self.close_dialog
-					),
-					MDRectangleFlatButton(
-						text = "Continue", on_release = self.continue_dialog
-					)
-				],
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Teal"
+        return Builder.load_file('login.kv')
 
-			)
+    def loggerchecker(self):
+        if self.root is not None:
+            self.root.ids.welcome_label.text = f'Sup {self.root.ids.user.text}!'
 
-		self.dialog.open()
+    def clearchecker(self):
+        if self.root is not None:
+            self.root.ids.welcome_label.text = "WELCOME"
+            self.root.ids.user.text = ""
+            self.root.ids.password.text = ""
 
-	def close_dialog(self, obj):
-		self.dialog.dismiss()
+    def show_alert_dialog(self):
+        if self.root is None:
+            return
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="LOGGED IN",
+                text=f"you have logged in {self.root.ids.user.text} ",
+                buttons=[
+                    MDFlatButton(
+                        text="Cancel", on_release=self.close_dialog
+                    ),
+                    MDRectangleFlatButton(
+                        text="Continue", on_release=self.continue_dialog
+                    )
+                ],
 
-	def continue_dialog(self, obj):
-		self.dialog.dismiss()
-		self.root.ids.welcome_label.text = "You have logged out"
-		self.root.ids.welcome_label.font_size = 20
+            )
 
-	
+        self.dialog.open()
+
+    def close_dialog(self, obj):
+        if self.dialog is None:
+            return
+        self.dialog.dismiss()
+
+    def continue_dialog(self, obj):
+        if self.dialog is None or self.root is None:
+            return
+        self.dialog.dismiss()
+        self.root.ids.welcome_label.text = "You have logged out"
+        self.root.ids.welcome_label.font_size = 20
+
+
 SchoolProjApp().run()
