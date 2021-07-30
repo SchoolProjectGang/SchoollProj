596c176741333cca3ff7bc7516994426338313f2
Wanderer0074348 <83384365+Wanderer0074348@users.noreply.github.com>
2021-07-30 08:21:44 +0530

    Update MysqlClass.py

--- kivy-login/MysqlClass.py
+++ kivy-login/MysqlClass.py
@@ -1,13 +1,16 @@
 import mysql.connector as con
 
 class Mysql:
-    def __init__(self, username, password, host) -> None:
-        self.username = username
-        self.password = password
-        self.host = host
+    def __init__(self) -> None:
+        self.username = 'root'
+        self.password = 't456tt456t'
+        self.host = 'localhost'
+    
+    # def main(self):
+    #     pass    # manages the class calls and everything
 
 
-    def connect(self):
+    def connect(self):                  
         self.m = con.connect(
             host = self.host,
             username = self.username,
@@ -27,8 +30,15 @@ class Mysql:
         self.cursor.execute(f"drop database {dbname}")
         self.m.commit()
         return
-
-userinput_u = input("enter your mysql username please thanks!")
-userinput_p = input("enter your mysql password please thanks!") 
-local_instance = Mysql(userinput_u, userinput_p, 'localhost')
-local_instance.connect()
+    def make_table(self, tbname, f1, f2, f3, f4, f5, f6):
+        self.cursor.execute(f"create table {tbname}({f1},{f2}, {f3}, {f4}, {f5}, {f6}) ")
+        self.m.commit()
+        return
+    def drop_table(self, tbname):
+        self.cursor.execute(f"drop table {tbname}")
+        self.m.commit()
+        return
+# userinput_u = input("enter your mysql username please thanks!")
+# userinput_p = input("enter your mysql password please thanks!") 
+# local_instance = Mysql(userinput_u, userinput_p, 'localhost')
+# local_instance.connect()
