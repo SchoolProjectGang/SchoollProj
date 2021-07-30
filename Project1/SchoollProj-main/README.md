833043892c9cbb72a45366e68bc074ed092897e4
Wanderer0074348 <83384365+Wanderer0074348@users.noreply.github.com>
2021-07-30 23:03:49 +0530

    Add files via upload

---
+++ Project1/SchoollProj-main/README.md
@@ -0,0 +1,17 @@
+DONT FORGET TO INSTALL KIVY AND KIVYMD PACKAGES USING PIP OR ANACONDA 
+ALSO DONT FORGET YOU NEED PYTHON, WINDOWS 10 OR UBUNTU OR ANY LINUX DISTRO
+
+
+RUN:(tested on)
+Windows - 
+
+          pip install kivy
+
+          pip install kivymd
+          
+Ubuntu/Arch/*NIX -  Many Ways( id still use pip to avoid brain dmg )
+          
+
+          pip3 isntall kivy 
+          
+          pip3 install kivymd
---
+++ Project1/SchoollProj-main/kivy-login/main.py
@@ -0,0 +1,74 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton
+from kivy.uix.screenmanager import ScreenManager, Screen
+from MysqlClass import Mysql
+
+
+class HistoryScreen(Screen):
+    pass
+
+
+class ListingsScreen(Screen):
+    pass
+
+
+class AccountCreation(Screen):
+    def account_made_in_db(self):
+        if self is None:
+            return
+        new_user = self.ids.new_use.text
+        new_pass = self.ids.new_password.text
+
+        x = Mysql()
+        x.add_userdata(new_user, new_pass)
+
+        self.ids.new_use.text = ""
+        self.ids.new_password.text = ""
+
+
+class BuyingScreen(Screen):
+    pass
+
+
+class MarketScreen(Screen):
+    pass
+
+
+class LoginScreen(Screen):
+
+    # show input fields
+    def clearchecker(self):
+        if self is not None:
+            self.ids.welcome_label.text = "WELCOME"
+            self.ids.user.text = ""
+            self.ids.password.text = ""
+
+    # login button, will update username and password and authenticate
+    def login_to_db(self):
+        if self is None:
+            return
+        self.username = self.ids.user.text
+        # print(username)
+        self.password = self.ids.password.text
+
+        self.ids.user.text = ""
+        self.ids.password.text = ""
+
+    def auth(self):
+        return Mysql().check_creds(self.username, self.password)
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Red"
+        return Builder.load_file('login.kv')
+
+
+MainApp().run()
