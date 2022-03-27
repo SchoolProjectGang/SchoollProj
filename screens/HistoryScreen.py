ba1586213a4e96797ea43f002678f83ce9a1be6d
Username-08 <yashranjan08@gmail.com>
2022-03-27 22:26:38 +0530

    add multithreading support for more buzzwords

--- screens/HistoryScreen.py
+++ screens/HistoryScreen.py
@@ -1,5 +1,8 @@
+import os
+from threading import Thread
 from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDFlatButton
 from kivy.uix.screenmanager import Screen
 from core.User_buy import UserData
 import cache.username
@@ -17,13 +20,23 @@ class HistoryScreen(Screen):
             for i in range(len(self.items)):
                 card = MDCard(
                     size_hint=(None, None),
-                    size=(1000, 25),
+                    size=(1000, 50),
                     pos_hint={"center_x": 1, "center_y": 1},
                     elevation=10,
                     padding=25,
                     spacing=25,
                     orientation="vertical",
                 )
-                label = MDLabel(text=f"{self.items[i]}")
+                label = MDFlatButton(
+                    text=f"{self.items[i]}", on_release=self.show_game_starting
+                )
                 card.add_widget(label)
                 self.ids["items"].add_widget(card)
+
+    def show_game_starting(self, instance):
+        new_thread = Thread(target=game_start, args=[])
+        new_thread.start()
+
+
+def game_start():
+    os.system("gnome-mahjongg")
